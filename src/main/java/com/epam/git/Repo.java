21ad46eea44d76a89dbfe97bfd54a8f4b56f1521
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-10-20 16:58:11 +0600

    https://github.com/sergeytsoy740/Module1-Task3-Git

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/username/reposotoryName";
+    public static String REPO_LINK = "https://github.com/sergeytsoy740/Module1-Task3-Git";
 }
